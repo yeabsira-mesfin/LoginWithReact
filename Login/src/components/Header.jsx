81f6980bc0e1ded5479075e932845f824c5223f4
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-12 06:24:10 -0400

    CSS module

--- Login/src/components/Header.jsx
+++ Login/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from '../assets/logo.png';
-import '../components/Header.css'
+import classes from  '../components/Header.module.css'
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
