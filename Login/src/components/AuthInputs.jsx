1afc575b564a3f64412bf852dc75b3a8f16e7c8c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-13 12:59:27 -0400

    Input with styled components

--- Login/src/components/AuthInputs.jsx
+++ Login/src/components/AuthInputs.jsx
@@ -17,6 +17,17 @@ const Label = styled.label`
   text-transform: uppercase;
   color: #6b7280;
 `
+
+const Input = styled.input`
+ width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -42,7 +53,7 @@ export default function AuthInputs() {
       <controlContainer>
         <p>
           <Label>Email</Label>
-          <input
+          <Input
             type="email"
             className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
@@ -50,7 +61,7 @@ export default function AuthInputs() {
         </p>
         <p>
           <Label>Password</Label>
-          <input
+          <Input
             type="password"
             className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
