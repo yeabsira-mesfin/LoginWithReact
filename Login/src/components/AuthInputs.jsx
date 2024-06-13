cf238dab30dab7d3da73fa53005bf535f6382c67
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-13 14:09:53 -0400

    Reusable input and labels

--- Login/src/components/AuthInputs.jsx
+++ Login/src/components/AuthInputs.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { styled } from "styled-components";
 import Button from "./Button";
+import Input from "./Input";
 const controlContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -8,26 +9,6 @@ const controlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label`
- display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid? '#f87171': '#6b7280'};
-`
-
-const Input = styled.input`
- width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color:  ${({$invalid})=> $invalid ? '#fed2d2': '#d1d5db'};
-  color: ${({$invalid})=> $invalid? '#374151': '#374151'};
-  border: 1px solid  ${({$invalid})=> $invalid?'#ef4444':'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
 
 
 export default function AuthInputs() {
@@ -53,24 +34,24 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <controlContainer>
-        <p>
-          <Label $invalid ={emailNotValid}>Email</Label>
+       
           <Input
+            label="Email"
             type="email"
             $invalid= {emailNotValid}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid = {emailNotValid}>Password</Label>
+        
+       
           <Input
+          label="Password"
             type="password"
              $invalid= {emailNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
-        </p>
+        
       </controlContainer>
       <div className="actions">
         <button type="button" className="text-button">
